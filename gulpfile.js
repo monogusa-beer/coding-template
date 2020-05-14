686132f5b2a7765738bb23799a693313dce0f8ee
楠欣士 <>
2020-05-14 10:32:47 +0900

    srcとdistにフォルダ分け ejsを設定 gulpfileの記述を変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const gulpSass = require("gulp-sass");
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
 const sassGlob = require("gulp-sass-glob");
@@ -7,25 +7,48 @@ const browserSync = require("browser-sync");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssdeclsort = require("css-declaration-sorter");
-const sourcemaps = require("gulp-sourcemaps");
-const imagemin = require("gulp-imagemin");
+const mqpacker = require("css-mqpacker");
+const gulpEjs = require("gulp-ejs");
+const rename = require("gulp-rename");
+const replace = require("gulp-replace");
+const gulpImagemin = require("gulp-imagemin");
 const pngquant = require("imagemin-pngquant");
 const mozjpeg = require("imagemin-mozjpeg");
 
-// scssのコンパイル
-gulp.task("sass", function () {
+const paths = {
+  sass: {
+    src: "./src/scss/style.scss",
+    dist: "./dist/css",
+  },
+  images: {
+    src: "./src/img/*.{png,jpg,gif,svg}",
+    dist: "./dist/img",
+  },
+  ejs: {
+    src: ["./src/ejs/**/*.ejs", "!" + "./src/ejs/**/_*.ejs"],
+    dist: "./dist",
+  },
+  js: {
+    src: "./src/js/script.js",
+    dist: "./dist/js",
+  },
+};
+
+// Sassのコンパイル
+function sass() {
   return gulp
-    .src("./scss/style.scss")
-    .pipe(sourcemaps.init())
+    .src(paths.sass.src, { sourcemaps: true })
     .pipe(
       plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
     )
     .pipe(sassGlob())
     .pipe(
-      sass({
+      gulpSass({
         outputStyle: "expanded",
       })
     )
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([cssdeclsort({ order: "alphabetical" })]))
     .pipe(
       postcss([
         autoprefixer({
@@ -34,56 +57,89 @@ gulp.task("sass", function () {
         }),
       ])
     )
-    .pipe(postcss([cssdeclsort({ order: "alphabetical" })]))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./css"));
-});
+    .pipe(gulp.dest(paths.sass.dist, { sourcemaps: "./" }));
+}
 
-// 保存時のリロード
-gulp.task("browser-sync", function (done) {
+// EJSのコンパイル
+function ejs(done) {
+  gulp
+    .src(paths.ejs.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
+    .pipe(gulpEjs())
+    .pipe(rename({ extname: ".html" }))
+    .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, "$1"))
+    .pipe(gulp.dest(paths.ejs.dist));
+  done();
+}
+
+// jsファイルのコピー
+function copyJs(done) {
+  gulp.src("./src/js/*.js").pipe(gulp.dest("./dist/js"));
+  done();
+}
+
+// ブラウザシンク
+function browsersync(done) {
   browserSync.init({
     server: {
-      baseDir: "./",
+      baseDir: "./dist",
       index: "index.html",
     },
   });
   done();
-});
-
-gulp.task("bs-reload", function (done) {
+}
+// 保存時のリロード
+function bsReload(done) {
   browserSync.reload();
   done();
-});
-
-// 監視
-gulp.task("watch", function (done) {
-  gulp.watch("./*.html", gulp.task("bs-reload"));
-  gulp.watch("./scss/**/*.scss", gulp.task("sass"));
-  gulp.watch("./scss/**/*.scss", gulp.task("bs-reload"));
-  gulp.watch("./js/*.js", gulp.task("bs-reload"));
-});
-
-// default
-gulp.task("default", gulp.series(gulp.parallel("browser-sync", "watch")));
+}
 
 //圧縮率の定義
 const imageminOption = [
   pngquant({ quality: [0.7, 0.85] }),
   mozjpeg({ quality: 85 }),
-  imagemin.gifsicle({
+  gulpImagemin.gifsicle({
     interlaced: false,
     optimizationLevel: 1,
     colors: 256,
   }),
-  imagemin.mozjpeg(),
-  imagemin.optipng(),
-  imagemin.svgo(),
+  gulpImagemin.mozjpeg(),
+  gulpImagemin.optipng(),
+  gulpImagemin.svgo(),
 ];
 
 // 画像の圧縮
-gulp.task("imagemin", function () {
+function imagemin() {
   return gulp
-    .src("./img/base/*.{png,jpg,gif,svg}")
-    .pipe(imagemin(imageminOption))
-    .pipe(gulp.dest("./img"));
-});
+    .src(paths.images.src)
+    .pipe(gulpImagemin(imageminOption))
+    .pipe(gulp.dest(paths.images.dist));
+}
+
+// 監視
+function watch(done) {
+  gulp.watch("./src/scss/**/*.scss", sass);
+  gulp.watch("./src/scss/**/*.scss", bsReload);
+  gulp.watch("./src/ejs/**/*.ejs", ejs);
+  gulp.watch("./src/ejs/**/*.ejs", bsReload);
+  gulp.watch("./src/js/*.js", copyJs);
+  gulp.watch("./src/js/*.js", bsReload);
+  done();
+}
+
+// exports
+exports.default = gulp.series(
+  sass,
+  ejs,
+  copyJs,
+  gulp.parallel(browsersync, watch)
+);
+exports.sass = sass;
+exports.ejs = ejs;
+exports.copyJs = copyJs;
+exports.browsersync = browsersync;
+exports.bsReload = bsReload;
+exports.imagemin = imagemin;
+exports.watch = watch;
